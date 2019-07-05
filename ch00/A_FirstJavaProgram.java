457adb53c8ca5d8c4138f2793b65cee64df01abe
ikarus920214 <ikarus920214@gmail.com>
2019-07-05 16:32:08 +0900

    챕터 00 01 예제 추가

---
+++ ch00/A_FirstJavaProgram.java
@@ -0,0 +1,7 @@
+public class A_FirstJavaProgram {
+	
+	public static void main(String[] args){
+		System.out.println("Welcom to Java");
+		System.out.println("First Java Program");
+	}
+}
---
+++ ch01/src/D_lineComment.java
@@ -0,0 +1,21 @@
+// 파일이름: LineComment.java
+// 작 성 자: 윤일노
+// 작 성 일: 2019년 7월 3일
+// 목    적: System.outprintln 메소드의 기능 테스트
+
+public class D_lineComment 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println(7);  //단순한 정수의 출력
+		
+		System.out.println(3.15);
+		System.out.println(3 + 5);    //덧셈 결과의 출력
+		
+		System.out.println("3 + 5" +" 의 연산 결과는 8 입니다.");
+		System.out.println("3 + 5 = " + 8);
+		System.out.println(3.15 + "는 실수입니다.");
+	}
+
+}
